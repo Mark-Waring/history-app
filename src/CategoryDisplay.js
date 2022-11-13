e41e8562eec11c8b40b037d0f1ab9613b28ea6c7
mark waring~ <markwaring00@gmail.com>
2022-11-12 21:50:00 -0600

    adds media query select bar

--- src/CategoryDisplay.js
+++ src/CategoryDisplay.js
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom"
+import { useMediaQuery } from '@mountain-ui/react-hooks';
 
 export default function CategoryDisplay () {
-    
+    const isMobile = useMediaQuery('screen and (min-width: 1px) and (max-width: 513px)');
+
     return (
+        <>
+        {!isMobile ? 
         <div className="category-container">
             <NavLink className="category" to="events">
                 Historical Events
@@ -20,6 +24,26 @@ export default function CategoryDisplay () {
                 Other Events
             </NavLink>
         </div>
+        : <select className="category-container">
+                <NavLink className="category" to="events">
+                    <option>Historical Events</option>
+                </NavLink>
+                <NavLink className="category" to="births">
+                    <option>Birthdays</option>
+                </NavLink>
+                <NavLink className="category" to="deaths">
+                    <option>Deaths</option>
+                </NavLink>
+                <NavLink className="category" to="holidays">
+                    <option>Holidays</option>
+                </NavLink>
+                <NavLink className="category" to="selected">
+                    <option>Other Events</option>
+                </NavLink>
+            </select>
+        
+        }
+        </>
     )
 }
 
