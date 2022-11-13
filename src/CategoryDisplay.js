e4afb201b3efe85b3b958020642e7402ed394d86
mark waring~ <markwaring00@gmail.com>
2022-11-13 16:19:47 -0600

    adds media query to categories

--- src/CategoryDisplay.js
+++ src/CategoryDisplay.js
@@ -1,23 +1,44 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 export default function CategoryDisplay() {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 600px)",
+  });
+  let navigate = useNavigate();
+
   return (
     <div className="category-container">
-      <NavLink className="category" to="events">
-        Historical Events
-      </NavLink>
-      <NavLink className="category" to="births">
-        Birthdays
-      </NavLink>
-      <NavLink className="category" to="deaths">
-        Deaths
-      </NavLink>
-      <NavLink className="category" to="holidays">
-        Holidays
-      </NavLink>
-      <NavLink className="category" to="selected">
-        Other Events
-      </NavLink>
+      {!isMobile ? (
+        <>
+          <NavLink className="category" to="events">
+            Historical Events
+          </NavLink>
+          <NavLink className="category" to="births">
+            Birthdays
+          </NavLink>
+          <NavLink className="category" to="deaths">
+            Deaths
+          </NavLink>
+          <NavLink className="category" to="holidays">
+            Holidays
+          </NavLink>
+          <NavLink className="category" to="selected">
+            Other Events
+          </NavLink>
+        </>
+      ) : (
+        <select
+          onChange={(e) => navigate(e.target.value)}
+          id="categories-dropdown"
+        >
+          <option value="events">Historical Events</option>
+          <option value="births">Birthdays</option>
+          <option value="deaths">Deaths</option>
+          <option value="holidays">Holidays</option>
+          <option value="selected">Other Events</option>
+        </select>
+      )}
     </div>
   );
 }
