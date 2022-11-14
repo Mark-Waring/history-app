4fac8a8f0c4f3cf5dedf383c13bd693d5a24977f
mark waring~ <markwaring00@gmail.com>
2022-11-14 16:23:58 -0600

    adds login form

--- src/CategoryDisplay.js
+++ src/CategoryDisplay.js
@@ -1,11 +1,9 @@
 import { NavLink, useNavigate, useParams } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
-import { useState, useEffect, useContext } from "react";
-import { AppContext } from "./AppContext";
+import { useState, useEffect } from "react";
 
 export default function CategoryDisplay() {
   const [mobileCat, setMobileCat] = useState("events");
-  const { selectedDate } = useContext(AppContext);
   const { category } = useParams();
   const isMobile = useMediaQuery({
     query: "(max-width: 600px)",
@@ -24,38 +22,35 @@ export default function CategoryDisplay() {
     setMobileCat(category);
   }, [category]);
 
-  if (!selectedDate) {
-    return;
+  if (!isMobile) {
+    return (
+      <div class="category-container">
+        {categories.map((val) => (
+          <NavLink className="category" key={val.val} to={val.val}>
+            {val.display}
+          </NavLink>
+        ))}
+      </div>
+    );
   }
-
   return (
     <div className="category-container">
-      {!isMobile ? (
-        <>
-          {categories.map((val) => (
-            <NavLink className="category" key={val.val} to={val.val}>
-              {val.display}
-            </NavLink>
-          ))}
-        </>
-      ) : (
-        <select
-          id="category"
-          name="category"
-          style={{ width: "40%", textAlign: "center" }}
-          value={mobileCat}
-          onChange={(e) => {
-            setMobileCat(e.target.value);
-            navigate(e.target.value);
-          }}
-        >
-          {categories.map((val) => (
-            <option key={val.val} value={val.val}>
-              {val.display}
-            </option>
-          ))}
-        </select>
-      )}
+      <select
+        id="category"
+        name="category"
+        style={{ width: "40%", textAlign: "center" }}
+        value={mobileCat}
+        onChange={(e) => {
+          setMobileCat(e.target.value);
+          navigate(e.target.value);
+        }}
+      >
+        {categories.map((val) => (
+          <option key={val.val} value={val.val}>
+            {val.display}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
